ac639d69d005f31729895a42cc736d194beab2b2
rinrinkarin <yycha613@gmail.com>
2026-05-13 11:44:56 -0500

    script

--- Assets/Scripts/scene01/Scene01Events.cs
+++ Assets/Scripts/scene01/Scene01Events.cs
@@ -8,7 +8,8 @@ public class Scene01Events : MonoBehaviour
     public GameObject fadeScreenIn;
     public GameObject CharMc;
     public GameObject textBox;
-
+    [SerializeField] GameObject nextButton;
+    [SerializeField] int EventPos = 0;
    
    
 
@@ -19,12 +20,29 @@ public class Scene01Events : MonoBehaviour
 
     IEnumerator EventStarter()
     {
+        // event 0
         yield return new WaitForSeconds(1);
         fadeScreenIn.SetActive(false);
         CharMc.SetActive(true);
         yield return new WaitForSeconds(1);
         // this is where our text function will go in future turotial
-    
+        nextButton.SetActive(true);
+        EventPos = 1;
+        
+    }
+
+    IEnumerator EventOne()
+    {
+        nextButton.SetActive(false);
         textBox.SetActive(true);
+      yield return new WaitForSeconds(1);
+    }
+
+    public void NextButton()
+    {
+        if (EventPos == 1)
+        {
+           StartCoroutine(EventOne());
+        }
     }
 }
